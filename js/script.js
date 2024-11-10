a0d49b55d510c1dc8b5c1093597cf7b4bc607738
huynhkhuan2506 <huynhkhuan777@gmail.com>
2024-11-10 19:01:20 +0700

    Remove all files

--- js/script.js
+++
@@ -1,8 +0,0 @@
-async function loadHTML(file, elementId) {
-    const response = await fetch(file);
-    const text = await response.text();
-    document.getElementById(elementId).innerHTML = text;
-}
-
-loadHTML('../../src/html/header.html', 'header-placeholder');
-loadHTML('/WebDesignFrontEnd/ProjectGiuaKy/src/html/footer.html', 'footer-placeholder');
